d4586234ec9ee58da49d8fd69384c0e27fd68f1b
altai861 <altaizandanbileg@gmail.com>
2023-08-02 11:56:56 +0800

    SOmewhat added some redux for the accessToken but need more examination and search

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './components/Homepage/Home'
 import Login from './components/Auth/Login'
 import SignUp from './components/Auth/SignUp'
+import { useSelector } from 'react-redux'
+
 
 function App() {
+  const token = useSelector((state) => state.token);
+  console.log(token);
+  let isAuth = token !== null ? true : false;
   return (
     <div className='app'>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navbar />}>
             <Route 
-                exact path="/" element={<Home />}
+                exact path="/" element={ isAuth ? <Home /> : <Navigate to="/login"/>}
             />
             <Route 
                 path="login" element={<Login />}
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,27 +1,80 @@
 import React from 'react'
 import { useState } from 'react'
+import { useSelector, useDispatch } from "react-redux"
+import { setLogin } from './AuthSlice'
+import { useNavigate } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify';
+
 
 const Login = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loginUser = async () => {
+    const apiUrl = "http://localhost:3500/auth/login";
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "username": username,
+        "password": password
+      })
+    }
+    try {
+      const response = await fetch(apiUrl, requestOptions);
+      if (!response.ok) {
+        const error_data = await response.json();
+        toast.error(error_data.message);
+        return
+      }
+      const success_data = await response.json();
+      console.log(success_data.accessToken);
+      dispatch(setLogin(success_data.accessToken))
+      navigate('/')
+
+    } catch (error) {
+      console.log(error.message)
+    }
+
+  }
+
 
   const handleLogin = (event) => {
     event.preventDefault();
     console.log("Login form submitted")
     // make post request to the server and if the user exists then, 
-    // log the user in, else return an error message  
+    // log the user in, else return an error message
+    loginUser()
   
   }
 
   return (
     <div className='log-in'>
+      <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          />
+          {/* Same as */}
+        <ToastContainer />
       <h1>Нэвтрэх</h1>
       <form className='log-in-form' onSubmit={handleLogin}>
         <input type="text" placeholder='Хэрэглэгчийн нэр' className='log-in-input' value={username} onChange={(e) => setUsername(e.target.value)} required></input>
         <input type="password" placeholder='Нууц үг' className='log-in-input' value={password} onChange={(e) => setPassword(e.target.value)} required></input>
         <input type="submit" value="Нэвтрэх" className='log-in-submit-button'></input>
       </form>
-      <p>Бүртгүүлсэх үү? <a href="/sign-up">Бүртгүүлэх</a></p>
+      <p>Бүртгүүлэх үү? <a href="/sign-up">Бүртгүүлэх</a></p>
     </div>
   )
 }
